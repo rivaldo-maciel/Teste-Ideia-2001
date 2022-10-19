945888aea7e8d0bc32b90c070d60f5b2409960ee
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-10-18 22:52:56 -0300

    feat: add menu icon

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -2,3 +2,8 @@ function activeLanguageSelect() {
   const selectBtn = document.querySelector(".options-container");
   selectBtn.classList.toggle("active");
 }
+
+function activeMenu() {
+  const menuIconContainer = document.querySelector(".menu-icon-container");
+  menuIconContainer.classList.toggle("active-menu")
+}
